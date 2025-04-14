c3f89339070517cc21ca386da54dea4ac5d2863f
DanialNajafi <145651640+DanialNajafi@users.noreply.github.com>
2025-04-14 18:19:21 +0200

    Update duration.ts

--- src/duration.ts
+++ src/duration.ts
@@ -1,4 +1,32 @@
 export function formatDuration(seconds: number): string {
-  console.log(seconds);
-  throw new Error("not implemented yet");
+  // Spezialfall: negatives Argument -> Exception
+  if (seconds < 0) {
+    throw new Error('Negative time is not valid!');
+  }
+
+  // Rundung von Nachkommastellen
+  seconds = Math.round(seconds);
+
+  if (seconds === 0) {
+    return '0s';
+  }
+
+  const hours = Math.floor(seconds / 3600);
+  seconds %= 3600;
+
+  const minutes = Math.floor(seconds / 60);
+  const sec = seconds % 60;
+
+  const parts: string[] = [];
+  if (hours > 0) {
+    parts.push(`${hours}h`);
+  }
+  if (minutes > 0) {
+    parts.push(`${minutes}m`);
+  }
+  if (sec > 0) {
+    parts.push(`${sec}s`);
+  }
+
+  return parts.join('');
 }
