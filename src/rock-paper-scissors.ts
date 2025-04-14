8c6f74af1d562013445b02e28276a328be8cfffe
DanialNajafi <145651640+DanialNajafi@users.noreply.github.com>
2025-04-14 18:21:55 +0200

    Update rock-paper-scissors.ts

--- src/rock-paper-scissors.ts
+++ src/rock-paper-scissors.ts
@@ -1,4 +1,3 @@
-export function play(player1: string, player2: string): number {
-  console.log(player1, player2);
-  throw new Error("not implemented yet");
+export function play(move1: string, move2: string): number {
+  return 0; // Platzhalter
 }
